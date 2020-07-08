4ffddbd13cc7ee57944128ec94df1701813c5239
kepop1 <kepop123@gmail.com>
2020-07-08 19:15:02 +0100

    Push up the handlebars files for V2 ready for further work

--- src/routes/html-routes.js
+++ src/routes/html-routes.js
@@ -4,13 +4,28 @@ const Food = require("../models/food");
 
 const router = express.Router();
 
-router.get("/view", function (req, res) {
+router.get("/deprecated_v1", (req, res) => {
   const onFindAll = (results) => {
     const devouredFoods = results.filter((result) => result.isDevoured === "1");
     const foods = results.filter((result) => result.isDevoured === "0");
     res.render("devour", { foods, devouredFoods });
   };
   Food.findAll({ raw: true }).then(onFindAll);
+});
+
+router.get("/restaurant_list", (req, res) => {
+  const foo = {};
+  res.render("restaurant_list", { foo });
+});
+
+router.get("/menu/:id", (req, res) => {
+  const foo = {};
+  res.render("menu", { foo });
+});
+
+router.get("/basket", (req, res) => {
+  const foo = {};
+  res.render("basket", { foo });
 });
 
 module.exports = router;
